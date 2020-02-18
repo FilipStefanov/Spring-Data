bb566f0bed8b077a010e311c98c129eeb35bdbaf
Filip Stefanov <48603123+FilipStefanov@users.noreply.github.com>
2020-02-18 12:31:35 +0200

    Add files via upload

---
+++ Writing_your_own_data_retrieval_application.java
@@ -0,0 +1,54 @@
+package SPRING_DATA;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Writing_your_own_data_retrieval_application {
+    public static void main(String[] args) throws SQLException {
+        Properties props = new Properties();
+
+        String user = "";
+        props.setProperty("user", "root");
+        String password = "";
+        props.setProperty("password", "1234");
+
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", props);
+
+        PreparedStatement statement = connection
+                .prepareStatement("SELECT \n" +
+                        "   u.user_name, u.first_name, u.last_name, COUNT(g.id) AS count\n" +
+                        "FROM\n" +
+                        "    users AS u\n" +
+                        "        LEFT JOIN\n" +
+                        "    users_games AS ug ON u.id = ug.user_id\n" +
+                        "        LEFT JOIN\n" +
+                        "    games AS g ON g.id = ug.game_id\n" +
+                        "WHERE\n" +
+                        "    u.user_name LIKE (?);");
+        Scanner scanner = new Scanner(System.in);
+
+        statement.setString(1, (scanner.nextLine()));
+        ResultSet results = statement.executeQuery();
+
+        while (results.next()) {
+
+            if(results.getString("first_name") == null) {
+
+                System.out.println("No such user exists");
+            }
+            else
+                System.out.println(String.format("User: %s \r\n%s %s has played %d games",
+
+                        results.getString("user_name"),
+                        results.getString("first_name"),
+                        results.getString("last_name"),
+                        results.getInt("count")
+
+                ));
+
+        }
+    }
+}
+
